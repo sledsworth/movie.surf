36859b95f4db133dae5cb0f4a042d8af231ecbc8
Steve Ledsworth <steve@ledsworth.com>
2025-01-06 21:33:18 -0500

    fix: buttons not working after initial page redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,31 @@ import { defineConfig } from "astro/config";
 
 import netlify from "@astrojs/netlify";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://movie.surf",
-	server: {
-		port: 3001,
+  site: "https://movie.surf",
+
+  server: {
+      port: 3001,
 	},
-	prefetch: true,
-	adapter: netlify(),
-	experimental: {
-		session: {
-			// The name of the unstorage driver is camelCase
-			driver: "netlify-blobs",
-			options: {
-				name: "movie-surf-sessions",
-				// Sessions need strong consistency
-				consistency: "strong",
-			},
-		},
-		contentIntellisense: true,
+
+  prefetch: true,
+  adapter: netlify(),
+
+  experimental: {
+      session: {
+          // The name of the unstorage driver is camelCase
+          driver: "netlify-blobs",
+          options: {
+              name: "movie-surf-sessions",
+              // Sessions need strong consistency
+              consistency: "strong",
+          },
+      },
+      contentIntellisense: true,
 	},
+
+  integrations: [svelte()],
 });
